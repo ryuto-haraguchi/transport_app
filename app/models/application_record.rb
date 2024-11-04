91fbb8456fdcfc50957adb7873b4dd6ddee16e62
haraguchi <hr48691226@gmail.com>
2024-11-04 17:05:15 +0900

    normalize_attributes_method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  before_validation :normalize_attributes
+
+  private
+
+  def normalize_attributes
+    attributes.each do |key, value|
+      if value.is_a?(String) 
+        self[key] = value.tr("０-９", "0-9").gsub(/[\s　]+/, "")
+      end
+    end
+  end
 end

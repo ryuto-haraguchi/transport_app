c9049b0778d10164e657081d1cf7f2709734380f
haraguchi <hr48691226@gmail.com>
2024-11-08 18:46:29 +0900

    employee_show_project

--- app/controllers/employees/projects_controller.rb
+++ app/controllers/employees/projects_controller.rb
@@ -1,2 +1,15 @@
 class Employees::ProjectsController < ApplicationController
+
+  def start_project
+    @project = Project.find(params[:id])
+    @project.update(status: "進行中", actual_collection_date: Time.now)
+    redirect_to employees_employee_path(current_employee)
+  end
+
+  def complete_project
+    @project = Project.find(params[:id])
+    @project.update(status: "完了", actual_delivery_date: Time.now)
+    redirect_to employees_employee_path(current_employee)
+  end
+
 end
